2441870b353d84e79e8d38a1b443eb17c22f916e
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-25 14:17:17 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,13 +5,12 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html = open ( "./fixtures/student-site/index.html")
-    
+    html = open("./fixtures/student-site/index.html")
+    doc = Nokogiri::HTML(html)
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
